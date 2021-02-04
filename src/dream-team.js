15ff3815c21b1764204485dc0575a9909cfe2548
Gena888 <theGena888@gmail.com>
2021-02-04 21:38:31 +0300

    feat: dream team ready

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false
+  let namesArr = []
+  let lettersArr = []
+  let sortedLettersArr = []
+  let result = ''
+
+  for (m = 0; m < members.length; m++) {
+    if (typeof members[m] == 'string') {
+      namesArr.push(members[m].toUpperCase())
+    }
+  }
+
+  for (n = 0; n < namesArr.length; n++) {
+    let firstChar = namesArr[n].charAt(0)
+    while (firstChar == ' ') {
+      let i = 0
+      firstChar = namesArr[n].charAt(i)
+      i++
+    }
+    lettersArr.push(firstChar)
+  }
+
+  sortedLettersArr = lettersArr.sort()
+  result = sortedLettersArr.join('')
+  return result
+
 };
