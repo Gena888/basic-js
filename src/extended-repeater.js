389e5d37927f6ea6a9687ecf0a099dc7d1778e8d
Gena888 <theGena888@gmail.com>
2021-02-11 17:23:30 +0300

    feat: extended-repeater ready

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  let stringStr = String(str)
+
+  let repeatTimesNum = options.repeatTimes
+  if (repeatTimesNum == undefined) { repeatTimesNum = 1 }
+  let separatorStr = options.separator
+  if (!separatorStr) { separatorStr = '+' }
+  let addition = String(options.addition)
+  let additionRepeatTimesNum = options.additionRepeatTimes
+  if (additionRepeatTimesNum == undefined) { additionRepeatTimesNum = 1 }
+  let additionSeparatorStr = options.additionSeparator
+  if (!additionSeparatorStr) { additionSeparatorStr = '|' }
+
+  let addStr = ''
+
+  let resultStr = ''
+
+  if (addition) {
+    let addArr = []
+    for (let i = 1; i <= additionRepeatTimesNum; i++) {
+      addArr.push(addition)
+      addArr.push(additionSeparatorStr)
+    }
+    addArr.pop()
+    addStr = addArr.join('')
+  }
+
+  if (stringStr) {
+    let strArr = []
+    for (let j = 1; j <= repeatTimesNum; j++) {
+      strArr.push(stringStr)
+      if (addStr !== 'undefined') {
+        strArr.push(addStr)
+      }
+      strArr.push(separatorStr)
+    }
+    strArr.pop()
+    resultStr = strArr.join('')
+  }
+
+  return resultStr
+
+
+
+
 };
-  
